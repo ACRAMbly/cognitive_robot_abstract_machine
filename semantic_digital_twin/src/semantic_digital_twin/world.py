daf3de6268af8209de3d928b0b8cd8da6dba4090
Tom Oltmanns <timol1904@gmail.com>
2026-07-07 22:16:33 +0200

    update world.py to fix circular import issue

--- semantic_digital_twin/src/semantic_digital_twin/world.py
+++ semantic_digital_twin/src/semantic_digital_twin/world.py
@@ -59,7 +59,6 @@ from semantic_digital_twin.exceptions import (
     MismatchingWorld,
 )
 from semantic_digital_twin.mixin import HasSimulatorProperties
-from semantic_digital_twin.robots.robot_parts import AbstractRobot
 from semantic_digital_twin.spatial_computations.forward_kinematics import (
     ForwardKinematicsManager,
 )
@@ -611,6 +610,7 @@ class World(HasSimulatorProperties):
 
     @property
     def robot_bodies_with_collision(self) -> List[Body]:
+        from semantic_digital_twin.robots.robot_parts import AbstractRobot
         return [
             body
             for robot in self.get_semantic_annotations_by_type(AbstractRobot)
@@ -618,7 +618,8 @@ class World(HasSimulatorProperties):
         ]
 
     @property
-    def robot_body_to_robot_mapping(self) -> dict[Body, AbstractRobot]:
+    def robot_body_to_robot_mapping(self) -> dict[Body, 'AbstractRobot']:
+        from semantic_digital_twin.robots.robot_parts import AbstractRobot
         return {
             body: robot
             for robot in self.get_semantic_annotations_by_type(AbstractRobot)
