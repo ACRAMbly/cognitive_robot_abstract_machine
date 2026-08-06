0e255856c8558da9c55933d376738f408c1162e7
Simon Stelter <stelter@uni-bremen.de>
2026-08-06 12:32:10 +0200

    add error details to `MissingGoalOutcomeError` and `MissingActionResultError`, update tests to validate error reporting, and refactor PR2 joint name usage in motion statechart tests

--- test/giskardpy_test/test_motion_statechart/test_wiggle_insert.py
+++ test/giskardpy_test/test_motion_statechart/test_wiggle_insert.py
@@ -14,13 +14,11 @@ from giskardpy.motion_statechart.monitors.overwrite_state_monitors import (
     SetSeedConfiguration,
 )
 from giskardpy.motion_statechart.monitors.payload_monitors import (
-    CountControlCycles,
     CountSimulationTimeSeconds,
 )
 from giskardpy.motion_statechart.motion_statechart import MotionStatechart
 from giskardpy.motion_statechart.tasks.cartesian_tasks import CartesianPosition
 from giskardpy.motion_statechart.tasks.wiggle_insert import WiggleInsert
-from giskardpy.ros_executor import Ros2Executor
 from semantic_digital_twin.adapters.ros.visualization.viz_marker import (
     VizMarkerPublisher,
 )
@@ -28,7 +26,6 @@ from semantic_digital_twin.datastructures.joint_state import JointState
 from semantic_digital_twin.spatial_types import Point3, Vector3
 from semantic_digital_twin.world import World
 from test.semantic_digital_twin_test.test_orm.test_orm import hsr_world_state_reset
-from semantic_digital_twin.robots.pr2 import PR2Joint
 
 
 def _hole_at_current_tip(world: World, tip, root) -> Point3:
@@ -147,8 +144,8 @@ def test_wiggle_insert(hsr_world_state_reset):
         "arm_flex_joint": -1.5,
         "arm_lift_joint": 0.5,
         "arm_roll_joint": 0.0,
-        PR2Joint.HEAD_PAN: 0.0,
-        PR2Joint.HEAD_TILT: 0.0,
+        "head_pan_joint": 0.0,
+        "head_tilt_joint": 0.0,
         "wrist_flex_joint": -1.5,
         "wrist_roll_joint": 0.0,
     }
